5396ed4a9a03b7bae53d76de04a00e97ebd854ac
Vladimir Perfilev <persoff68@gmail.com>
2021-01-15 11:52:15 +0300

    TDM-75 WIP

--- src/main/java/com/persoff68/fatodo/repository/ChatRepository.java
+++ src/main/java/com/persoff68/fatodo/repository/ChatRepository.java
@@ -29,57 +29,63 @@ public interface ChatRepository extends JpaRepository<Chat, UUID> {
             """)
     Page<Chat> findAllByUserId(UUID userId, Pageable pageable);
 
-    @Query("""
-            select c from Chat c join c.memberEvents as m
-            where m.userId = ?1 and c.lastModifiedAt > ?2
-            order by c.lastModifiedAt desc
-            """)
-    List<Chat> findAllNewByUserId(UUID userId, Date date);
 
     @Query(value = """
             with unified as (
-                select   m.id, m.chat_id, created_at as timestamp, null as type
-                from     ftd_chat_message as m
-                right join ftd_chat_member_event as e on m.chat_id = e.chat_id
-                where    e.user_id = ?1
+                select m.id, m.chat_id, created_at as timestamp, null as type
+                from ftd_chat_message as m
+                         right join ftd_chat_member_event as e on m.chat_id = e.chat_id
+                where e.user_id = ?1
                 union
-                select   id, chat_id, timestamp, type
-                from     ftd_chat_member_event
-                where    user_id = ?1),
-
-            validated_message as (
-                select   id, chat_id, type, timestamp,
-                         sum(case type when 'ADD_MEMBER' then 1 when 'DELETE_MEMBER' then -1 else 0 end)
-                            over (order by timestamp rows unbounded preceding) *
-                         min(case type when 'CLEAR_HISTORY' then 0 when 'DELETE_DIALOG' then 0 else 1 end)
-                            over (order by timestamp rows between current row and unbounded following) valid
-                from     unified),
-                
-            last_message_id as (
-                select distinct last_value(id)
-                                    over (partition by chat_id order by timestamp) id
-                from     validated_message
-                where    type is null and valid = 1),
-                
-            last_message as (
-                select * 
-                from ftd_chat_message
-                where id in (select id from last_message_id)),
-                
-            validated_chat as (
-                select   chat_id,
-                         last_value(type) over (partition by chat_id order by timestamp) last_event
-                from     unified),
-                
-            chat_id as (
-                select distinct chat_id
-                from validated_chat
-                where last_event <> 'DELETE_DIALOG'
-            )
+                select id, chat_id, timestamp, type
+                from ftd_chat_member_event
+                where user_id = ?1),
             
-            select c.*, m.id as last_allowed_message_id 
+             validated_message as (
+                 select id,
+                        chat_id,
+                        type,
+                        timestamp,
+                        sum(case
+                                when type like 'ADD_MEMBER' then 1
+                                when type like 'DELETE_MEMBER' then -1
+                                else 0
+                                end)
+                            over (order by timestamp rows unbounded preceding) *
+                        min(case
+                                when type like 'CLEAR_HISTORY' then 0
+                                when type like 'DELETE_DIALOG' then 0
+                                else 1
+                                end)
+                            over (order by timestamp rows between current row and unbounded following) valid
+                 from unified),
+            
+             last_message_id as (
+                 select distinct last_value(id)
+                                            over (partition by chat_id order by timestamp) id
+                 from validated_message
+                 where type is null
+                   and valid = 1),
+            
+             last_message as (
+                 select *
+                 from ftd_chat_message
+                 where id in (select id from last_message_id)),
+            
+             validated_chat as (
+                 select chat_id,
+                        last_value(type) over (partition by chat_id order by timestamp) last_event
+                 from unified),
+            
+             chat_id as (
+                 select distinct chat_id as id
+                 from validated_chat
+                 where last_event not like 'DELETE_DIALOG'
+             )
+            
+            select c.*, m.id as last_allowed_message_id
             from ftd_chat as c
-            join last_message as m on c.id = m.chat_id
+            left join last_message as m on c.id = m.chat_id
             where c.id in (select id from chat_id)
             order by m.created_at desc
             """, nativeQuery = true)
--- src/test/java/com/persoff68/fatodo/service/ChatServiceIT.java
+++ src/test/java/com/persoff68/fatodo/service/ChatServiceIT.java
@@ -3,15 +3,23 @@ package com.persoff68.fatodo.service;
 import com.persoff68.fatodo.FatodoMessageServiceApplication;
 import com.persoff68.fatodo.builder.TestChat;
 import com.persoff68.fatodo.builder.TestMemberEvent;
+import com.persoff68.fatodo.builder.TestMessage;
 import com.persoff68.fatodo.model.Chat;
 import com.persoff68.fatodo.model.MemberEvent;
+import com.persoff68.fatodo.model.Message;
 import com.persoff68.fatodo.repository.ChatRepository;
 import com.persoff68.fatodo.repository.MemberRepository;
+import com.persoff68.fatodo.repository.MessageRepository;
+import com.persoff68.fatodo.service.exception.ModelNotFoundException;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.context.WebApplicationContext;
 
 import java.util.Arrays;
@@ -19,6 +27,7 @@ import java.util.List;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 
 @SpringBootTest(classes = FatodoMessageServiceApplication.class)
@@ -35,32 +44,71 @@ public class ChatServiceIT {
     @Autowired
     MemberRepository memberRepository;
     @Autowired
+    MessageRepository messageRepository;
+    @Autowired
     ChatService chatService;
 
     MockMvc mvc;
 
     @BeforeEach
-    public void setup() throws Exception {
+    public void setup() {
         mvc = MockMvcBuilders.webAppContextSetup(context).apply(springSecurity()).build();
 
         memberRepository.deleteAll();
         chatRepository.deleteAll();
 
-        Chat chat = saveChat(USER_1_ID, USER_2_ID);
-        saveChat(USER_2_ID, USER_3_ID);
+        Chat firstChat = saveChat(USER_1_ID, USER_2_ID);
         saveChat(USER_1_ID, USER_3_ID);
-        saveChat(USER_1_ID);
+        saveChat(USER_2_ID, USER_3_ID);
+        for (int i = 0; i < 100; i++) {
+            saveMessage(firstChat.getId(), USER_1_ID, UUID.randomUUID().toString());
+        }
+//        List<Message> messageList = messageRepository.findAllByChatId(firstChat.getId());
+//        firstChat.setMessages(messageList);
+//        chatRepository.save(firstChat);
     }
 
-    private Chat saveChat(UUID... userIds) {
+    @Test
+    @Transactional
+    public void getAllByUserIdTest() {
+        Pageable pageable = PageRequest.of(0, 100);
+        List<Chat> chatList = chatService.getAllByUserId(USER_1_ID, pageable);
+        int size = chatList.stream()
+                .map(chat -> chat.getMessages().size())
+                .reduce(Integer::sum)
+                .orElse(0);
+        List<Message> messageList = messageRepository.findAll();
+        assertThat(messageList.size()).isEqualTo(100);
+        assertThat(chatList.size()).isEqualTo(2);
+        assertThat(size).isEqualTo(100);
+    }
+
+    @Transactional
+    protected Chat saveChat(UUID... userIds) {
         Chat chat = TestChat.defaultBuilder().build().toParent();
         chat = chatRepository.save(chat);
         UUID chatId = chat.getId();
         List<MemberEvent> memberEventList = Arrays.stream(userIds)
-                .map(userId -> TestMemberEvent.defaultBuilder().chatId(chatId).userId(userId).build().toParent())
+                .map(userId -> TestMemberEvent.defaultBuilder()
+                        .chatId(chatId)
+                        .userId(userId)
+                        .type(MemberEvent.Type.ADD_MEMBER)
+                        .build().toParent())
                 .collect(Collectors.toList());
         chat.setMemberEvents(memberEventList);
         return chatRepository.save(chat);
+    }
+
+    @Transactional
+    protected Message saveMessage(UUID chatId, UUID userId, String text) {
+        Chat chat = chatRepository.findById(chatId)
+                .orElseThrow(ModelNotFoundException::new);
+        Message message = TestMessage.defaultBuilder()
+                .chat(chat)
+                .userId(userId)
+                .text(text)
+                .build().toParent();
+        return messageRepository.save(message);
     }
 
 }
