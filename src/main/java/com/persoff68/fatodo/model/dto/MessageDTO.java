ffd60d6da8b73f6c54e65dac68bb68acb4ef8840
Vladimir Perfilev <persoff68@gmail.com>
2022-07-15 13:52:57 +0200

    TDM-149 delete serializable from classes

--- src/main/java/com/persoff68/fatodo/model/dto/MessageDTO.java
+++ src/main/java/com/persoff68/fatodo/model/dto/MessageDTO.java
@@ -1,19 +1,15 @@
 package com.persoff68.fatodo.model.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.persoff68.fatodo.config.constant.AppConstants;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
-import java.io.Serial;
 import java.util.Set;
 import java.util.UUID;
 
 @Data
 @EqualsAndHashCode(callSuper = true)
 public class MessageDTO extends AbstractAuditingDTO {
-    @Serial
-    private static final long serialVersionUID = AppConstants.SERIAL_VERSION_UID;
 
     private UUID chatId;
     private UUID userId;
--- src/main/java/com/persoff68/fatodo/model/dto/StatusDTO.java
+++ src/main/java/com/persoff68/fatodo/model/dto/StatusDTO.java
@@ -1,18 +1,13 @@
 package com.persoff68.fatodo.model.dto;
 
-import com.persoff68.fatodo.config.constant.AppConstants;
 import com.persoff68.fatodo.model.constant.StatusType;
 import lombok.Data;
 
-import java.io.Serial;
-import java.io.Serializable;
 import java.util.Date;
 import java.util.UUID;
 
 @Data
-public class StatusDTO implements Serializable {
-    @Serial
-    private static final long serialVersionUID = AppConstants.SERIAL_VERSION_UID;
+public class StatusDTO {
 
     private UUID messageId;
     private UUID userId;
