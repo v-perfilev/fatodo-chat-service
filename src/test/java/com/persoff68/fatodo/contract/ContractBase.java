44684c8d3ab2ffb7051bf362db36607ad08ed1fe
Vladimir Perfilev <persoff68@gmail.com>
2022-07-23 18:40:51 +0200

    TDM-150 fix contact base

--- src/test/java/com/persoff68/fatodo/contract/ContractBase.java
+++ src/test/java/com/persoff68/fatodo/contract/ContractBase.java
@@ -20,6 +20,7 @@ import com.persoff68.fatodo.repository.MessageRepository;
 import com.persoff68.fatodo.repository.ReactionRepository;
 import com.persoff68.fatodo.repository.StatusRepository;
 import io.restassured.module.mockmvc.RestAssuredMockMvc;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -32,13 +33,12 @@ import javax.persistence.EntityManager;
 import java.util.UUID;
 
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.when;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
 @AutoConfigureMessageVerifier
 @Transactional
-abstract class ContractBase {
+class ContractBase {
     private static final UUID USER_ID_1 = UUID.fromString("8f9a7cae-73c8-4ad6-b135-5bd109b51d2e");
     private static final UUID USER_ID_2 = UUID.fromString("1b53a48c-2da5-4489-ac8a-e246c6445333");
     private static final UUID CHAT_ID = UUID.fromString("b73e8418-ff4a-472b-893d-4e248ae93797");
@@ -74,12 +74,6 @@ abstract class ContractBase {
     void setup() {
         RestAssuredMockMvc.webAppContextSetup(context);
 
-        chatRepository.deleteAll();
-        messageRepository.deleteAll();
-        memberEventRepository.deleteAll();
-        reactionRepository.deleteAll();
-        statusRepository.deleteAll();
-
         Chat chat = createChat("test", CHAT_ID);
         createAddMemberEvent(chat, USER_ID_1);
         createAddMemberEvent(chat, USER_ID_2);
@@ -90,17 +84,15 @@ abstract class ContractBase {
         when(userServiceClient.doesIdExist(any())).thenReturn(true);
         when(userServiceClient.doIdsExist(any())).thenReturn(true);
         when(contactServiceClient.areUsersInContactList(any())).thenReturn(true);
+    }
 
-        doNothing().when(wsServiceClient).sendChatNewEvent(any());
-        doNothing().when(wsServiceClient).sendChatUpdateEvent(any());
-        doNothing().when(wsServiceClient).sendChatLastMessageEvent(any());
-        doNothing().when(wsServiceClient).sendChatLastMessageUpdateEvent(any());
-        doNothing().when(wsServiceClient).sendMessageNewEvent(any());
-        doNothing().when(wsServiceClient).sendMessageUpdateEvent(any());
-        doNothing().when(wsServiceClient).sendStatusesEvent(any());
-        doNothing().when(wsServiceClient).sendReactionsEvent(any());
-        doNothing().when(eventServiceClient).addChatEvent(any());
-        doNothing().when(eventServiceClient).deleteChatEventsForUser(any());
+    @AfterEach
+    void cleanup() {
+        chatRepository.deleteAll();
+        messageRepository.deleteAll();
+        memberEventRepository.deleteAll();
+        reactionRepository.deleteAll();
+        statusRepository.deleteAll();
     }
 
     private Chat createChat(String title, UUID chatId) {
